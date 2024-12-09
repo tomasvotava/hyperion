724e29cab89afb575dce4bf28e71cac3e1f9bf3a
Tomas Votava <info@tomasvotava.eu>
2024-12-09 15:18:41 +0100

    feat: add meters to degrees conversion function for geographical calculations

--- hyperion/infrastructure/geo/location.py
+++ hyperion/infrastructure/geo/location.py
@@ -1,9 +1,19 @@
 """Common location classes and functions."""
 
+import math
 from dataclasses import dataclass
 
+LATITUDE_DEGREE_TO_METERS = 111_000
 
-@dataclass
+
+def meters_to_degrees(meters: float, at_latitude: float) -> tuple[float, float]:
+    """Convert meters to degrees at a given latitude."""
+    return meters / LATITUDE_DEGREE_TO_METERS, meters / (
+        LATITUDE_DEGREE_TO_METERS * math.cos(math.radians(at_latitude))
+    )
+
+
+@dataclass(frozen=True, eq=True)
 class Location:
     """A geographical location."""
 
