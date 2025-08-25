6390322b901d8fd5de7ef0fe19010d2440fd8205
Tomas Votava <info@tomasvotava.eu>
2025-08-25 20:38:50 +0200

    feat: make sources able to run independently from lambda (e.g. in Argo Workflows), add optional `params` to Source
    This adds FileQueue that sources can use to notify about new assets locally. The flushed queue can then be used by another consumer to further process the assets.

--- hyperion/infrastructure/message_queue.py
+++ hyperion/infrastructure/message_queue.py
@@ -1,8 +1,13 @@
 import abc
 import datetime
+import json
+import shutil
+import sys
+import tempfile
 from collections.abc import Iterator
 from enum import Enum
-from typing import ClassVar
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, ClassVar
 
 from aws_lambda_typing.events import SQSEvent
 from boto3 import client
@@ -15,6 +20,12 @@ from hyperion.config import config, queue_config
 from hyperion.dateutils import utcnow
 from hyperion.entities.catalog import DataLakeAsset
 from hyperion.log import get_logger
+
+if sys.version_info >= (3, 11) and TYPE_CHECKING:
+    from typing import Self
+
+if sys.version_info < (3, 11) and TYPE_CHECKING:
+    from typing_extensions import Self
 
 logger = get_logger("hyperion-queue")
 
@@ -51,6 +62,11 @@ class SourceBackfillMessage(Message):
     start_date: datetime.datetime | datetime.date
     end_date: datetime.datetime | datetime.date | None = None
     notify: bool = True
+
+
+class SerializedMessage(BaseModel):
+    message_type: str
+    message: str
 
 
 def iter_messages_from_sqs_event(event: SQSEvent) -> Iterator[Message]:
@@ -90,13 +106,31 @@ def create_backfill_event(
 
 
 class Queue(abc.ABC):
+    _cached_instance: ClassVar["Queue | None"] = None
+
     @staticmethod
-    def from_config() -> "Queue":
+    def _create_from_config() -> "Queue":
+        if queue_config.url and queue_config.path:
+            logger.warning(
+                "Ambiguous configuration detected - both queue URL and queue path were set. Will default to SQS queue.",
+                queue_url=queue_config.url,
+                queue_path=queue_config.path,
+            )
+        if queue_config.url is not None:
+            logger.info("Using SQS queue.")
+            return SQSQueue(queue_config.url)
+        if queue_config.path is not None:
+            logger.info("Using FileQueue.")
+            return FileQueue(Path(queue_config.path), overwrite=queue_config.path_overwrite)
         if queue_config.url is None:
             logger.info("Using in-memory queue.")
             return InMemoryQueue()
-        logger.info("Using SQS queue.")
-        return SQSQueue(queue_config.url)
+
+    @classmethod
+    def from_config(cls) -> "Queue":
+        if cls._cached_instance is None:
+            cls._cached_instance = cls._create_from_config()
+        return cls._cached_instance
 
     @abc.abstractmethod
     def send(self, message: Message) -> None:
@@ -149,3 +183,77 @@ class SQSQueue(Queue):
 
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__} {self._queue_url}>"
+
+
+class FileQueue(Queue):
+    def __init__(self, queue_path: Path, *, overwrite: bool = False, exclusive: bool = False) -> None:
+        """File-based message queue.
+
+        If the queue is exclusive and it is entered multiple times, a RuntimeError will be raised.
+
+        Args:
+            queue_path (Path): The path to the queue file.
+            overwrite (bool, optional): Whether to overwrite the queue file if it exists. Defaults to False.
+            exclusive (bool, optional): Whether to use exclusive access to the queue file. Defaults to False.
+        """
+        super().__init__()
+        if queue_path.exists():
+            if not queue_path.resolve().is_file():
+                raise FileExistsError(f"Queue path {queue_path} already exists and is not a file.")
+            if not overwrite:
+                raise FileExistsError(f"Queue path {queue_path} already exists, set overwrite=True to ignore this.")
+            logger.warning("Queue path already exists, it will be overwritten.", queue_path=queue_path, queue=self)
+        self._messages: list[Message] = []
+        self.queue_path = queue_path.resolve()
+        self._context_stacklevel = 0
+        self._exclusive = exclusive
+
+    def send(self, message: Message) -> None:
+        if self._context_stacklevel == 0:
+            logger.warning("Using FileQueue outside of a context manager does not persist the messages.", queue=self)
+        self._messages.append(message)
+
+    def delete(self, receipt_handle: str) -> None:
+        raise NotImplementedError("FileQueue does not implement message deletion.")
+
+    def _flush(self) -> None:
+        serializable_list = [
+            {"message_type": message.__class__.__name__, "message": message.model_dump_json()}
+            for message in self._messages
+        ]
+        with tempfile.NamedTemporaryFile("w", delete=False) as tmp_file:
+            json.dump(serializable_list, tmp_file)
+        shutil.move(tmp_file.name, self.queue_path)
+        logger.info(
+            f"Flushed {len(self._messages)} from FileQueue.",
+            queue=self,
+            messages=len(self._messages),
+            queue_path=self.queue_path,
+        )
+        self._messages.clear()
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} at {hex(id(self))} exclusive={self._exclusive} path={self.queue_path}>"
+
+    def __enter__(self) -> "Self":
+        if self._exclusive and self._context_stacklevel != 0:
+            raise RuntimeError(
+                "FileQueue's context has already been entered, this cannot be done twice in exclusive mode."
+            )
+        self._context_stacklevel += 1
+        return self
+
+    def __exit__(self, *args: Any, **kwargs: Any) -> None:
+        self._context_stacklevel -= 1
+        if self._context_stacklevel != 0:
+            logger.info("FileQueue's context was exited, but there is still an open context somewhere, skipping flush.")
+            return
+        self._flush()
+
+    @staticmethod
+    def iter_messages_from_file(queue_file: Path) -> Iterator[Message]:
+        with queue_file.open("r", encoding="utf-8") as file:
+            raw_messages = json.load(file)
+        for raw_message in raw_messages:
+            serialized_message = SerializedMessage.model_validate(raw_message)
+            yield Message.deserialize(serialized_message.message, serialized_message.message_type)
