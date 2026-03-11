d8841651e678d13e45ccb82f874f9f02981ebed0
Tomas Votava <info@tomasvotava.eu>
2026-03-11 16:47:42 +0000

    feat: remove AsyncHTTPClientWrapper, promote `get_proxy_mounts` to public method

--- hyperion/infrastructure/httputils.py
+++ hyperion/infrastructure/httputils.py
@@ -1,5 +1,3 @@
-import asyncio
-from types import TracebackType
 from urllib.parse import urlparse
 
 import httpx
@@ -20,49 +18,24 @@ def redact_url(url: str, replace: str = "***") -> str:
     return url
 
 
-class AsyncHTTPClientWrapper:
-    def __init__(self) -> None:
-        self._client: httpx.AsyncClient | None = None
-        self._stacklevel = 0
-        self._lock = asyncio.Lock()
+def get_proxy_mounts() -> dict[str, httpx.AsyncHTTPTransport]:
+    """Build httpx proxy mounts from HYPERION_HTTP_PROXY_HTTP / HYPERION_HTTP_PROXY_HTTPS env vars.
 
-    async def __aenter__(self) -> httpx.AsyncClient:
-        async with self._lock:
-            self._stacklevel += 1
-            if self._client is None or self._client.is_closed:
-                logger.debug("Creating and entering httpx async client.")
-                self._client = httpx.AsyncClient(mounts=self._get_proxy_mounts())
-                await self._client.__aenter__()
-        return self._client
-
-    async def __aexit__(
-        self,
-        exc_type: type[BaseException] | None = None,
-        exc_value: BaseException | None = None,
-        traceback: TracebackType | None = None,
-    ) -> None:
-        async with self._lock:
-            self._stacklevel -= 1
-            if self._stacklevel == 0 and self._client is not None:
-                logger.info("Closing httpx async client.")
-                await self._client.__aexit__(exc_type, exc_value, traceback)
-                self._client = None
-
-    @staticmethod
-    def _get_proxy_mounts() -> dict[str, httpx.AsyncHTTPTransport]:
-        proxy_mounts: dict[str, httpx.AsyncHTTPTransport] = {}
-        if http_config.proxy_http:
-            redacted_url = redact_url(http_config.proxy_http)
-            logger.info("Configuring HTTP proxy for http://", proxy_url=redacted_url)
-            proxy_mounts["http://"] = httpx.AsyncHTTPTransport(proxy=http_config.proxy_http)
-            if not http_config.proxy_https:
-                logger.info("Configuring HTTP proxy for https://", proxy_url=redacted_url)
-                proxy_mounts["https://"] = httpx.AsyncHTTPTransport(proxy=http_config.proxy_http)
-        if http_config.proxy_https:
-            redacted_url = redact_url(http_config.proxy_https)
+    If only one of the two is set, it is used for both http:// and https://.
+    """
+    proxy_mounts: dict[str, httpx.AsyncHTTPTransport] = {}
+    if http_config.proxy_http:
+        redacted_url = redact_url(http_config.proxy_http)
+        logger.info("Configuring HTTP proxy for http://", proxy_url=redacted_url)
+        proxy_mounts["http://"] = httpx.AsyncHTTPTransport(proxy=http_config.proxy_http)
+        if not http_config.proxy_https:
             logger.info("Configuring HTTP proxy for https://", proxy_url=redacted_url)
-            proxy_mounts["https://"] = httpx.AsyncHTTPTransport(proxy=http_config.proxy_https)
-            if not http_config.proxy_http:
-                logger.info("Configuring HTTP proxy for http://", proxy_url=redacted_url)
-                proxy_mounts["http://"] = httpx.AsyncHTTPTransport(proxy=http_config.proxy_https)
-        return proxy_mounts
+            proxy_mounts["https://"] = httpx.AsyncHTTPTransport(proxy=http_config.proxy_http)
+    if http_config.proxy_https:
+        redacted_url = redact_url(http_config.proxy_https)
+        logger.info("Configuring HTTP proxy for https://", proxy_url=redacted_url)
+        proxy_mounts["https://"] = httpx.AsyncHTTPTransport(proxy=http_config.proxy_https)
+        if not http_config.proxy_http:
+            logger.info("Configuring HTTP proxy for http://", proxy_url=redacted_url)
+            proxy_mounts["http://"] = httpx.AsyncHTTPTransport(proxy=http_config.proxy_https)
+    return proxy_mounts
